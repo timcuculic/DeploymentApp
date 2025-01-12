6b809b6dfd431e721d8062b691acc6f08f9311af
Tim Cuculic <timcuculic@hotmail.com>
2025-01-12 03:03:25 -0500

    publish on Sunday morn at 3 am

--- DeploymentApp/Models/Person.cs
+++ DeploymentApp/Models/Person.cs
@@ -1,11 +1,15 @@
-﻿namespace DeploymentApp.Models
+﻿using System.ComponentModel;
+
+namespace DeploymentApp.Models
 {
     public class Person
     {
         public int PersonId { get; set; }
 
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
     }
 }
